3901ba36432f65d1a9bc6ca5d2c602baca686a99
Giao U <lesliegiao.u@gmail.com>
2025-06-27 08:14:34 -0700

    modify program.cs to reflect changes in Fish endpoints and fish type endpoints.

--- FishShop/Program.cs
+++ FishShop/Program.cs
@@ -1,12 +1,8 @@
-using System.ComponentModel.DataAnnotations;
 using FishShop.Data;
-using FishShop.Features.Fishes.CreateFish;
-using FishShop.Features.Fishes.DeleteFish;
-using FishShop.Features.Fishes.GetFish;
-using FishShop.Features.Fishes.GetFishes;
-using FishShop.Features.Fishes.UpdateFish;
+using FishShop.Features.Fishes;
+using FishShop.Features.FishesTypes;
 using FishShop.Features.FishesTypes.GetFishTypes;
-using FishShop.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -14,14 +10,10 @@ var app = builder.Build();
 FishData data = new();
 
 //Fish Endpoints
-app.MapGetFishes(data);
-app.MapGetFish(data);
-app.MapCreateFish(data);
-app.MapUpdateFish(data);
-app.MapDeleteFish(data);
+app.MapFishes(data);
 
 //FishType Endpoints
-app.MapGetFishTypes(data);
+app.MapFishTypes(data);
 
 
 app.Run();

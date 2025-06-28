8560d775577fab81672d2b49d4b6491a05008bd3
Giao U <lesliegiao.u@gmail.com>
2025-06-28 10:09:24 -0700

    Generate database migrations

---
+++ FishShop/Data/Migrations/20250628165626_InitialCreate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FishShop.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "types",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_types", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "fishes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    FishTypeId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Habitat = table.Column<string>(type: "TEXT", nullable: false),
+                    MaxSizeInInches = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    KoiFish = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_fishes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_fishes_types_FishTypeId",
+                        column: x => x.FishTypeId,
+                        principalTable: "types",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_fishes_FishTypeId",
+                table: "fishes",
+                column: "FishTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "fishes");
+
+            migrationBuilder.DropTable(
+                name: "types");
+        }
+    }
+}

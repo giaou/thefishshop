fef29e46c3c5afbc685b2035b0eee1d084cb1b27
Giao U <lesliegiao.u@gmail.com>
2025-07-01 21:55:12 -0700

    Retrieve all fish records from the database

--- FishShop/Features/Fishes/GetFishes/GetFishesEndpoint.cs
+++ FishShop/Features/Fishes/GetFishes/GetFishesEndpoint.cs
@@ -1,5 +1,7 @@
 using System;
 using FishShop.Data;
+using FishShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FishShop.Features.Fishes.GetFishes;
 
@@ -8,13 +10,16 @@ public static class GetFishesEndpoint
 
     public static void MapGetFishes(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/", (FishData data) => data.GetFishes().Select(fish => new FishSummaryDto(
+        app.MapGet("/", (FishDataContext dbContext) => dbContext.fishes
+                                                        .Include(fish => fish.Type)
+                                                        .Select(fish => new FishSummaryDto(
             fish.Id,
             fish.Name,
             fish.Type!.Name,
             fish.Habitat,
             fish.MaxSizeInInches,
-            fish.Price)));
+            fish.Price))
+                        .AsNoTracking());
     }
 
 }

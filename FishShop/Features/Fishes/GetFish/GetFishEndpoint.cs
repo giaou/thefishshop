56afeeab1d2ab8c452ebf0891ed59774069f1fda
Giao U <lesliegiao.u@gmail.com>
2025-07-16 22:05:16 -0700

    crash the program to start working with error handling

--- FishShop/Features/Fishes/GetFish/GetFishEndpoint.cs
+++ FishShop/Features/Fishes/GetFish/GetFishEndpoint.cs
@@ -1,14 +1,16 @@
 using FishShop.Data;
 using FishShop.Features.Fishes.Constants;
 using FishShop.Models;
+using Microsoft.Data.Sqlite;
 
 namespace FishShop.Features.Fishes.GetFish;
 
 public static class GetFishEndpoint
 {
     public static void MapGetFish(this IEndpointRouteBuilder app){
-        app.MapGet("/{id}", async (Guid id, FishDataContext dbContext) =>{
-            Fish? fish = await dbContext.fishes.FindAsync(id);
+        app.MapGet("/{id}", async (Guid id, FishDataContext dbContext) =>
+        {
+            Fish? fish = await FindFishAsync(id, dbContext);
             return fish is null ? Results.NotFound() : Results.Ok(
                 new FishDetailsDto(
                     fish.Id,
@@ -21,7 +23,13 @@ public static class GetFishEndpoint
                     fish.KoiFish
                 )
             );
-    
+
         }).WithName(EndpointName.GetFish);
+    }
+
+    private static async Task<Fish> FindFishAsync(Guid id, FishDataContext dbContext)
+    {
+        throw new SqliteException("The database is not available", 14);
+        return await dbContext.fishes.FindAsync(id);
     }
 }
